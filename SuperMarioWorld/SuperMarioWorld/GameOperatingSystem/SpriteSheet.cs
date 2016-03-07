53d97e4e2d48a77293cdfc1f6a94bb4db1ebae90
Pim Schwippert <p.schwippert@gmail.com>
2016-03-07 15:05:58 +0100

    Halp meeeee

--- SuperMarioWorld/SuperMarioWorld/GameOperatingSystem/SpriteSheet.cs
+++ SuperMarioWorld/SuperMarioWorld/GameOperatingSystem/SpriteSheet.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace SuperMarioWorld.GameOperatingSystem
 {
-    class SpriteSheet
+    public class SpriteSheet
     {
+        private Bitmap spriteSheet;
+
+        public SpriteSheet()
+        {
+
+
+        }
+
+        public void setSpriteSheet(Bitmap spriteSheet)
+        {
+            this.spriteSheet = spriteSheet;
+        }
+
+        public BufferedGraphics getTile(int xTile, int yTile, int width, int height)
+        {
+            BufferedGraphics sprite = spriteSheet.get(xTile, yTile, width, height);
+            return sprite;
+        }
     }
 }
