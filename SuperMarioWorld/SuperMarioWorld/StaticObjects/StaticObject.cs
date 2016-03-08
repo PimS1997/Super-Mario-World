aa7290102cfbe35048214ffc2550678dcb156a7f
KeijiTadashi <brian.bel47@gmail.com>
2016-03-08 12:02:44 +0100

    Added removed items again

---
+++ SuperMarioWorld/SuperMarioWorld/StaticObjects/StaticObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarioWorld
+{
+    abstract class StaticObject : GameObject
+    {
+
+        bool isBreakable;
+        bool isSolid;
+
+        public StaticObject() : base()
+        {
+
+        }
+
+    }
+}
